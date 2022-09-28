28b84a6310dc2afbbf427397d1eb6f18495aa43a
JoeZiminski <joseph.j.ziminski@gmail.com>
2022-09-28 17:22:09 +0100

    updated tests to use tmp_path for local folder and specify remote in conftest.py

---
+++ manager/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from pathlib import Path
+
+@pytest.fixture(scope="session", autouse=True)
+def remote_path():
+    """
+    """
+    remote_path = Path(r"Z:\manager\project_manager_tests")  # Set remote path here
+
+    if remote_path.name == "REMOTE PATH":
+        pytest.fail("[Test Setup Error]: remote_path is not set in conftest.py")
+
+    if not remote_path.exists():
+        pytest.fail("[Test Setup Error] remote_path (set in conftest.py) is not an existing directory.")
+
+    is_empty = next(remote_path.iterdir(), True)
+
+    if is_empty is not True:
+        pytest.fail("[Test Setup Error] remote_path (set in conftest.py) directory specified is not empty.")
+
+    remote_path.name
+
+    return remote_path
+
+
+
