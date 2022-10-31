454ab89ee31fd6fc22856e149e506ce107197b3e
JoeZiminski <Joseph.j.ziminski@gmail.com>
2022-10-31 19:03:57 +0000

    started adding cli

---
+++ datashuttle/__main__.py
@@ -0,0 +1,12 @@
+"""RP To-Do entry point script."""
+# rptodo/__main__.py
+
+import datashuttle.command_line_interface as cli
+
+
+def main():
+    cli.entry()
+
+
+if __name__ == "__main__":
+    main()
