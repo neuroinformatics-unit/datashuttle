61a2b021d9b7bb8dfe79aee65c115e194b642815
adamltyson <adamltyson@users.noreply.github.com>
2023-05-31 16:20:16 +0000

    deploy: d91c5553dde74caf3c047c73211cc52ae49733c2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gd0f2c52',
+    VERSION: '0.1.dev1+gd91c555',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
