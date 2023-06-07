cfc898166ab141d26ec15464d69b21d3405718b4
JoeZiminski <JoeZiminski@users.noreply.github.com>
2023-06-07 15:07:52 +0000

    deploy: 0b5095d3dcb3e7c79b606a0e392fd19065d377bd

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g2630bdf',
+    VERSION: '0.1.dev1+g0b5095d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
