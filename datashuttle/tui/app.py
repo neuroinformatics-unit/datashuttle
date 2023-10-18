8f0ba41741ffd0289f09374d973e2fa0d5a2c2b2
b-peri <brandon.peri@outlook.com>
2023-10-18 20:32:11 +0100

    Implemented all basic functions of Create tab. This tab now takes sub- and ses-level inputs + contains a checkbox for datatype selection. Clicking Make Folders runs make_sub_folders using these inputs and updates the DirectoryTree widget. Sub and ses input boxes can now also be automatically populated by double-clicking sub- and ses-level directories on the DirectoryTree, respectively

--- datashuttle/tui/app.py
+++ datashuttle/tui/app.py
@@ -3,15 +3,51 @@ from time import monotonic
 
 from textual.app import App, ComposeResult
 from textual.binding import Binding
+from textual.reactive import reactive
 from textual.widgets import (
+    Button,
+    Checkbox,
     DirectoryTree,
     Footer,
     Header,
     Input,
     Label,
+    Static,
     TabbedContent,
     TabPane,
 )
+
+from datashuttle.datashuttle import DataShuttle
+
+project = DataShuttle(
+    "test_project"
+)  # Change this to any local DataShuttle project for testing!
+
+
+class TypeBox(Static):
+    """
+    Dynamically-populated checkbox widget for convenient datatype selection during folder creation.
+    """
+
+    type_config = [
+        k.removeprefix("use_")
+        for k, v in zip(project.cfg.data.keys(), project.cfg.data.values())
+        if "use_" in k and v is True
+    ]
+
+    def compose(self):
+        for type in self.type_config:
+            yield Checkbox(type.title(), id=type, value=1)
+
+    type_out = reactive("all")
+
+    def on_checkbox_changed(self, event: Checkbox.Changed):
+        type_dict = {
+            type: self.query_one(f"#{type}").value for type in self.type_config
+        }
+        self.type_out = [
+            x for x, v in zip(type_dict.keys(), type_dict.values()) if v == 1
+        ]
 
 
 class TuiApp(App):
@@ -45,12 +81,18 @@ class TuiApp(App):
         yield Header()
         with TabbedContent():
             with TabPane("Create", id="create"):
-                yield DirectoryTree(str(Path().home()), id="FileTree")
-                yield Label("Folder Name")
-                yield Input(
-                    placeholder="Double-click on any folder to fill this field.",
-                    id="subject",
+                yield DirectoryTree(
+                    str(project.cfg.data["local_path"]), id="FileTree"
                 )
+                yield Label("Subject(s)", id="sub_label")
+                yield Input(id="subject", placeholder="e.g. sub-001")
+
+                # delete/move before committing
+                yield Label("Session(s)")
+                yield Input(id="session", placeholder="e.g. ses-001")
+                yield Label("Datatype(s)")
+                yield TypeBox()
+                yield Button("Make Folders", id="make_folder")
             with TabPane("Transfer", id="transfer"):
                 yield Label("Transfer; Seems to work!")
         yield Footer()
@@ -66,9 +108,24 @@ class TuiApp(App):
         """
 
         click_time = monotonic()
-        if click_time - self.prev_click_time < 0.5:
-            self.query_one("#subject").value = str(event.path)
-        self.prev_click_time = click_time
+
+        if click_time - self.prev_time < 0.5:
+            if event.path.stem.startswith("sub-"):
+                self.query_one("#subject").value = str(event.path.stem)
+            if event.path.stem.startswith("ses-"):
+                self.query_one("#session").value = str(event.path.stem)
+        self.prev_time = click_time
+
+    def on_button_pressed(self, event: Button.Pressed):
+        if event.button.id == "make_folder":
+            sub_dir = self.query_one("#subject").value
+            ses_dir = self.query_one("#session").value
+            project.make_sub_folders(
+                sub_names=sub_dir,
+                ses_names=ses_dir,
+                datatype=self.query_one("TypeBox").type_out,
+            )
+            self.query_one("#FileTree").reload()
 
 
 if __name__ == "__main__":
