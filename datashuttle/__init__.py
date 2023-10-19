a289d568f343b9c816216a1002fba46df6e6e1e9
JoeZiminski <Joseph.j.ziminski@gmail.com>
2023-10-19 19:36:34 +0100

    Shorten DataShuttle import path.

--- datashuttle/__init__.py
+++ datashuttle/__init__.py
@@ -1,7 +1,9 @@
 from importlib.metadata import PackageNotFoundError, version
 
+from datashuttle.datashuttle import DataShuttle
+
 try:
-    __version__ = version("{{cookiecutter.package_name}}")
+    __version__ = version("{{datashuttle.package_name}}")
 except PackageNotFoundError:
     # package is not installed
     pass
