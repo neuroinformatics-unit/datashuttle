0bc0b2ba2b02eaf04f3443d26461feb7ddcdbe55
JoeZiminski <Joseph.j.ziminski@gmail.com>
2023-10-19 19:38:20 +0100

    Fix __version__ attribute.

--- datashuttle/__init__.py
+++ datashuttle/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import PackageNotFoundError, version
 from datashuttle.datashuttle import DataShuttle
 
 try:
-    __version__ = version("{{datashuttle.package_name}}")
+    __version__ = version("datashuttle")
 except PackageNotFoundError:
     # package is not installed
     pass
