65854498940c16d0a0ec54bff0e0a56ed4398aad
b-peri <brandon.peri@outlook.com>
2023-10-20 10:33:26 +0100

    Removed comments or moved to docstring. Reformatting docstrings to enforce line limits. Clarified prev_time var name.

--- datashuttle/tui/app.py
+++ datashuttle/tui/app.py
@@ -28,17 +28,20 @@ class TuiApp(App):
 
     TITLE = "DataShuttle"
 
-    # Set path to TCSS
     tui_path = Path(__file__).parent
     CSS_PATH = tui_path / "css" / "tab_content.tcss"
 
-    # Set key-bindings
     BINDINGS = [
         Binding("ctrl+d", "toggle_dark", "Toggle Dark Mode", priority=True)
     ]
 
-    # Compose window
+    prev_click_time = 0.0
+
     def compose(self) -> ComposeResult:
+        """
+        Composes widgets to the TUI in the order specified.
+        """
+
         yield Header()
         with TabbedContent():
             with TabPane("Create", id="create"):
@@ -52,20 +55,20 @@ class TuiApp(App):
                 yield Label("Transfer; Seems to work!")
         yield Footer()
 
-    prev_time = 0.0
-
     def on_directory_tree_directory_selected(
         self, event: DirectoryTree.DirectorySelected
     ):
         """
-        After double-clicking a directory within the directory-tree widget, replaces contents of the \'Subject\'
-        Input widget with directory name
+        After double-clicking a directory within the directory-tree
+        widget, replaces contents of the \'Subject\' input widget
+        with directory name. Double-click time is set to the
+        Windows default (500 ms).
         """
 
         click_time = monotonic()
         if click_time - self.prev_time < 0.5:
             self.query_one("#subject").value = str(event.path)
-        self.prev_time = click_time
+        self.prev_click_time = click_time
 
 
 if __name__ == "__main__":
