7d09bbd1320ab5fb6f6a225570b56eedf1c2130a
Brandon Peri <77279592+b-peri@users.noreply.github.com>
2023-10-23 10:33:21 +0100

    [TUI] Add DirectoryTree and Input widgets to Create Tab (2) (#215)
    * Implemented DirectoryTree and functional text field on Create tab. Double clicking on a directory on the DirectoryTree prints the path to the text field.
    
    * Removed comments or moved to docstring. Reformatting docstrings to enforce line limits. Clarified prev_time var name.
    
    * Fixed bug with prev_click_time variable name

--- datashuttle/tui/app.py
+++ datashuttle/tui/app.py
@@ -1,8 +1,17 @@
 from pathlib import Path
+from time import monotonic
 
 from textual.app import App, ComposeResult
 from textual.binding import Binding
-from textual.widgets import Footer, Header, Label, TabbedContent, TabPane
+from textual.widgets import (
+    DirectoryTree,
+    Footer,
+    Header,
+    Input,
+    Label,
+    TabbedContent,
+    TabPane,
+)
 
 
 class TuiApp(App):
@@ -19,26 +28,48 @@ class TuiApp(App):
 
     TITLE = "DataShuttle"
 
-    # Set path to TCSS
-    tui_path = Path(__file__).parents[0]
+    tui_path = Path(__file__).parent
     CSS_PATH = tui_path / "css" / "tab_content.tcss"
 
-    # Set key-bindings
     BINDINGS = [
         Binding("ctrl+d", "toggle_dark", "Toggle Dark Mode", priority=True)
     ]
 
-    # Compose window
+    prev_click_time = 0.0
+
     def compose(self) -> ComposeResult:
+        """
+        Composes widgets to the TUI in the order specified.
+        """
+
         yield Header()
         with TabbedContent():
             with TabPane("Create", id="create"):
-                yield Label("Create", id="create_text")
+                yield DirectoryTree(str(Path().home()), id="FileTree")
+                yield Label("Folder Name")
+                yield Input(
+                    placeholder="Double-click on any folder to fill this field.",
+                    id="subject",
+                )
             with TabPane("Transfer", id="transfer"):
                 yield Label("Transfer; Seems to work!")
         yield Footer()
 
+    def on_directory_tree_directory_selected(
+        self, event: DirectoryTree.DirectorySelected
+    ):
+        """
+        After double-clicking a directory within the directory-tree
+        widget, replaces contents of the \'Subject\' input widget
+        with directory name. Double-click time is set to the
+        Windows default (500 ms).
+        """
+
+        click_time = monotonic()
+        if click_time - self.prev_click_time < 0.5:
+            self.query_one("#subject").value = str(event.path)
+        self.prev_click_time = click_time
+
 
 if __name__ == "__main__":
-    app = TuiApp()
-    app.run()
+    TuiApp().run()
