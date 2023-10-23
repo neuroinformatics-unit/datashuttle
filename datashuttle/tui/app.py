1bf0f4e12b44c94990bfbd8bfc7e581616d3ed41
Brandon Peri <77279592+b-peri@users.noreply.github.com>
2023-10-23 15:50:37 +0100

    [TUI] Implement Create Tab Functions (3) (#216)
    * Implemented all basic functions of Create tab. This tab now takes sub- and ses-level inputs + contains a checkbox for datatype selection. Clicking Make Folders runs make_sub_folders using these inputs and updates the DirectoryTree widget. Sub and ses input boxes can now also be automatically populated by double-clicking sub- and ses-level directories on the DirectoryTree, respectively
    
    * Double-clicking a ses-level directory now populates both sub- and ses- Input fields
    
    * Cleaned up code and re-implemented fixes from previous PR
    
    * Fixed .tcss to be able to display Checkboxes when n_datatypes < 3
    
    * Fixed bug in double-click time variables
    
    * Update TUI script to reflect changes to make_sub_folders and DataShuttle import path
    
    * Cleaned up irrelevant comments
    
    * working.
    
    * Store `project` as a TuiApp class attribute.
    
    * Rename `k`, `v` to `key`, `value`.
    
    * Remove acessing `UserDict` through `.data` attribute.
    
    * Removed unused event parameter.
    
    * Add docstring to `TypeBox.on_checkbox_changed().`
    
    * Rename variables in `TypeOut.on_checkbox_changed()`.
    
    * Re-rename variables for `TypeBox.type_config` creation.
    
    * Add docstring to class `TypeBox`.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add error handling widget for test phase.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Rename errors input widget.
    
    * Minor fix docstring
    
    * Edited `on_button_pressed` method so that FileTree only reloads when `make_folders` was successful
    
    ---------
    
    Co-authored-by: JoeZiminski <joseph.j.ziminski@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- datashuttle/tui/app.py
+++ datashuttle/tui/app.py
@@ -3,15 +3,72 @@ from time import monotonic
 
 from textual.app import App, ComposeResult
 from textual.binding import Binding
+from textual.reactive import reactive
 from textual.widgets import (
+    Button,
+    Checkbox,
     DirectoryTree,
     Footer,
     Header,
     Input,
     Label,
+    Static,
     TabbedContent,
     TabPane,
 )
+
+from datashuttle import DataShuttle
+
+
+class TypeBox(Static):
+    """
+    Dynamically-populated checkbox widget for convenient datatype
+    selection during folder creation.
+
+    Parameters
+    ---------
+
+    project_config: ConfigsClass
+        Configuration dictionary from datashuttle (i.e. `project.cfg`).
+
+    Attributes
+    ----------
+
+    type_out:
+        List of datatypes (e.g. "behav" that will be passed to `make-folders`.)
+
+    type_config:
+        List of datatypes that were set as 'True' during datashuttle project setup
+    """
+
+    type_out = reactive("all")
+
+    def __init__(self, project_cfg):
+        super(TypeBox, self).__init__()
+
+        self.type_config = [
+            config.removeprefix("use_")
+            for config, is_on in zip(project_cfg.keys(), project_cfg.values())
+            if "use_" in config and is_on
+        ]
+
+    def compose(self):
+        for type in self.type_config:
+            yield Checkbox(type.title(), id=type, value=1)
+
+    def on_checkbox_changed(self):
+        """
+        When a checkbox is clicked, update the `type_out` attribute
+        with the datatypes to pass to `make_folders` datatype argument.
+        """
+        type_dict = {
+            type: self.query_one(f"#{type}").value for type in self.type_config
+        }
+        self.type_out = [
+            datatype
+            for datatype, is_on in zip(type_dict.keys(), type_dict.values())
+            if is_on
+        ]
 
 
 class TuiApp(App):
@@ -37,19 +94,29 @@ class TuiApp(App):
 
     prev_click_time = 0.0
 
+    # Change this to any local DataShuttle project for testing!
+    project = DataShuttle("test_project")
+
     def compose(self) -> ComposeResult:
         """
         Composes widgets to the TUI in the order specified.
         """
-
         yield Header()
         with TabbedContent():
             with TabPane("Create", id="create"):
-                yield DirectoryTree(str(Path().home()), id="FileTree")
-                yield Label("Folder Name")
+                yield DirectoryTree(
+                    str(self.project.cfg.data["local_path"]), id="FileTree"
+                )
+                yield Label("Subject(s)", id="sub_label")
+                yield Input(id="subject", placeholder="e.g. sub-001")
+                yield Label("Session(s)")
+                yield Input(id="session", placeholder="e.g. ses-001")
+                yield Label("Datatype(s)")
+                yield TypeBox(self.project.cfg)
+                yield Button("Make Folders", id="make_folder")
                 yield Input(
-                    placeholder="Double-click on any folder to fill this field.",
-                    id="subject",
+                    id="errors_on_create_page",
+                    placeholder="Errors are printed here.",
                 )
             with TabPane("Transfer", id="transfer"):
                 yield Label("Transfer; Seems to work!")
@@ -59,16 +126,38 @@ class TuiApp(App):
         self, event: DirectoryTree.DirectorySelected
     ):
         """
-        After double-clicking a directory within the directory-tree
-        widget, replaces contents of the \'Subject\' input widget
-        with directory name. Double-click time is set to the
-        Windows default (500 ms).
+        Enables double-clicking a directory within the directory-tree
+        widget to replace contents of the \'Subject\' and/or \'Session\'
+        input widgets depending on the prefix of the directory selected.
+        Double-click time is set to the Windows default duration (500 ms).
         """
-
         click_time = monotonic()
         if click_time - self.prev_click_time < 0.5:
-            self.query_one("#subject").value = str(event.path)
+            if event.path.stem.startswith("sub-"):
+                self.query_one("#subject").value = str(event.path.stem)
+            if event.path.stem.startswith("ses-"):
+                self.query_one("#session").value = str(event.path.stem)
+                self.query_one("#subject").value = str(event.path.parent.stem)
         self.prev_click_time = click_time
+
+    def on_button_pressed(self, event: Button.Pressed):
+        """
+        Enables the Make Folder button to read out current input values
+        and use these to call project.make_folders().
+        """
+        if event.button.id == "make_folder":
+            sub_dir = self.query_one("#subject").value
+            ses_dir = self.query_one("#session").value
+
+            try:
+                self.project.make_folders(
+                    sub_names=sub_dir,
+                    ses_names=ses_dir,
+                    datatype=self.query_one("TypeBox").type_out,
+                )
+                self.query_one("#FileTree").reload()
+            except BaseException as e:
+                self.query_one("#errors_on_create_page").value = str(e)
 
 
 if __name__ == "__main__":
