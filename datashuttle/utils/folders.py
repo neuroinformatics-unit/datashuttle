735724afb17e5bc1bddb299f6196d4ddb9e02a67
JoeZiminski <joseph.j.ziminski@gmail.com>
2023-10-31 14:09:01 +0000

    Refactor and tidy up `get_next_sub_or_ses_number()`.

--- datashuttle/utils/folders.py
+++ datashuttle/utils/folders.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Dict
+from typing import TYPE_CHECKING, Dict, Literal
 
 if TYPE_CHECKING:
     from datashuttle import DataShuttle
@@ -580,29 +580,48 @@ def get_next_sub_or_ses_number(
     sub: Optional[str],
     search_str: str,
     return_with_prefix: bool = True,
+    default_num_value_digits: int = 3,
 ) -> str:
     """
     Suggest the next available subject or session number. This function will
     search the local repository, and the central repository, for all subject
     or session folders (subject or session depending on inputs).
+
     It will take the union of all folder names, find the relevant key-value
     pair values, and return the maximum value + 1 as the new number.
+
     A warning will be shown if the existing sub / session numbers are not
     consecutive.
 
     Parameters
     ----------
-    cfg : datashuttle configs class
-    sub : subject name to search within if searching for sessions, otherwise None
-          to search for subjects
-    search_str : the string to search for within the top-level or subject-levle
-                 folder ("sub-*") or ("ses-*") are suggested, respectively.
+    cfg : Configs
+        datashuttle configs class
+
+    sub : Optional[str]
+        subject name to search within if searching for sessions, otherwise None
+        to search for subjects
+
+    search_str : str
+        the string to search for within the top-level or subject-level
+        folder ("sub-*") or ("ses-*") are suggested, respectively.
+
+    return_with_prefix : bool
+        If `True`, the next sub or ses value will include the prefix
+        e.g. "sub-001", otherwise the value alone will be returned (e.g. "001").
+
+    default_num_value_digits : int
+        If no sub or ses exist in the project, the starting number is 1.
+        Because the number of digits for the project is not accessible,
+        the desired value can be entered here. e.g. if 3 (the default),
+        if no subjects are found the subject returned will be "sub-001".
+
     Returns
     -------
-    suggested_new_num : the new suggested sub / ses number.
-    latest_existing_num : the latest sub / ses number that currently
-                          exists in the project.
+    suggested_new_num : the new suggested sub / ses.
     """
+    bids_key: Literal["sub", "ses"]
+
     if sub:
         bids_key = "ses"
     else:
@@ -614,12 +633,61 @@ def get_next_sub_or_ses_number(
         search_str,
     )
 
-    # Convert subject values to a list of increasing-by-1 integers
     all_folders = list(set(folder_names["local"] + folder_names["central"]))
 
+    (
+        max_existing_num,
+        num_value_digits,
+    ) = get_max_sub_or_ses_num_and_value_length(
+        all_folders, bids_key, default_num_value_digits
+    )
+
+    # calculate next sub number
+    suggested_new_num = max_existing_num + 1
+    format_suggested_new_num = str(suggested_new_num).zfill(num_value_digits)
+
+    if return_with_prefix:
+        format_suggested_new_num = f"{bids_key}-{format_suggested_new_num}"
+
+    return format_suggested_new_num
+
+
+def get_max_sub_or_ses_num_and_value_length(
+    all_folders: List[str],
+    bids_key: Literal["sub", "ses"],
+    default_num_value_digits: int,
+) -> Tuple[int, int]:
+    """
+    Given a list of BIDS-style folder names, find the maximum subject or
+    session value (sub or ses depending on `bids_key`). Also, find the
+    number of value digits across the project, so a new suggested number
+    can be formatted consistency.
+
+    Parameters
+    ----------
+
+    all_folders : List[str]
+        A list of BIDS-style formatted folder names.
+
+    see `get_next_sub_or_ses_number()` for other arguments.
+
+    Returns
+    -------
+
+    max_existing_num : int
+        The largest number sub / ses value in the past list.
+
+    num_value_digits : int
+        The length of the value in all sub / ses values within the
+        passed list. If these are not consistent, an error is raised.
+
+    For example, if the project contains "sub-0001", "sub-0002" then
+    the max_existing_num will be 2 and num_value_digits 4.
+
+    """
     if len(all_folders) == 0:
-        latest_existing_num = 0
-        num_value_digits = 3  # TODO: this sets an important default!
+        max_existing_num = 0
+        num_value_digits = default_num_value_digits
     else:
         all_values_str = utils.get_values_from_bids_formatted_name(
             all_folders,
@@ -627,15 +695,19 @@ def get_next_sub_or_ses_number(
             return_as_int=False,
         )
 
+        # First get the length of bids-key value across the project
+        # (e.g. sub-003 has three values).
         all_num_value_digits = [len(value) for value in all_values_str]
+
         if not len(set(all_num_value_digits)) == 1:
-            # TODO: add as NeuroBLueprintError
+            # TODO: raise NeuroBlueprintError after PR #247
             raise Exception(
-                f"The number of value digits for the {bids_key} level are not consistent."
-                f"Cannot suggest a {bids_key} number."
+                f"The number of value digits for the {bids_key} level are not "
+                f"consistent. Cannot suggest a {bids_key} number."
             )
         num_value_digits = all_num_value_digits[0]
 
+        # Then get the latest existing sub or ses number in the project.
         all_value_nums = sorted(
             [utils.sub_or_ses_value_to_int(value) for value in all_values_str]
         )
@@ -646,16 +718,9 @@ def get_next_sub_or_ses_number(
                 f"currently used subject numbers are: {all_value_nums}"
             )
 
-        latest_existing_num = max(all_value_nums)
+        max_existing_num = max(all_value_nums)
 
-    # calculate next sub number
-    suggested_new_num = latest_existing_num + 1
-    format_suggested_new_num = str(suggested_new_num).zfill(num_value_digits)
-
-    if return_with_prefix:
-        format_suggested_new_num = f"{bids_key}-{format_suggested_new_num}"
-
-    return format_suggested_new_num
+    return max_existing_num, num_value_digits
 
 
 def get_existing_project_paths_and_names() -> Tuple[List[str], List[Path]]:
