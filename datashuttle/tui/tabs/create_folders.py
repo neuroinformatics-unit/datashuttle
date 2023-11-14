0d72ff1aa87bd425cc5814550e3e37a93fabeacc
JoeZiminski <joseph.j.ziminski@gmail.com>
2023-11-14 19:57:49 +0000

    Add validation to subject and session Inputs.

--- datashuttle/tui/tabs/create_folders.py
+++ datashuttle/tui/tabs/create_folders.py
@@ -1,5 +1,8 @@
 from time import monotonic
+from typing import Optional
 
+from textual.containers import Horizontal
+from textual.validation import ValidationResult, Validator
 from textual.widgets import (
     Button,
     DirectoryTree,
@@ -9,6 +12,7 @@ from textual.widgets import (
 )
 
 from datashuttle.tui import custom_widgets
+from datashuttle.utils import formatting, validation
 
 
 class CreateFoldersTab(TabPane):
@@ -23,7 +27,11 @@ class CreateFoldersTab(TabPane):
         )
         self.mainwindow = mainwindow
         self.project = project
+
         self.prev_click_time = 0.0
+
+        # TODO: move
+        self.default_input_tooltip = ""
 
     def compose(self):
         yield DirectoryTree(
@@ -31,12 +39,25 @@ class CreateFoldersTab(TabPane):
             id="tabscreen_directorytree",
         )
         yield Label("Subject(s)", id="tabscreen_subject_label")
-        yield Input(id="tabscreen_subject_input", placeholder="e.g. sub-001")
+        yield Input(
+            id="tabscreen_subject_input",
+            placeholder="e.g. sub-001",
+            validate_on=["changed", "submitted"],
+            validators=[QuickNeuroBlueprintValidator("sub", self)],
+        )  # TODO: what is validate_on["blur"]?
         yield Label("Session(s)", id="tabscreen_session_label")
-        yield Input(id="tabscreen_session_input", placeholder="e.g. ses-001")
+        yield Input(
+            id="tabscreen_session_input",
+            placeholder="e.g. ses-001",
+            validate_on=["changed", "submitted"],
+            validators=[QuickNeuroBlueprintValidator("ses", self)],
+        )
         yield Label("Datatype(s)", id="tabscreen_datatype_label")
         yield custom_widgets.DatatypeCheckboxes()
-        yield Button("Make Folders", id="tabscreen_make_folder_button")
+        yield Horizontal(
+            Button("Make Folders", id="tabscreen_make_folder_button"),
+            Button("Create Options", id="tabscreen_create_options_button"),
+        )
 
     def on_directory_tree_directory_selected(
         self, event: DirectoryTree.DirectorySelected
@@ -47,7 +68,6 @@ class CreateFoldersTab(TabPane):
         input widgets, depending on the prefix of the directory selected.
         Double-click time is set to the Windows default duration (500 ms).
         """
-
         click_time = monotonic()
         if click_time - self.prev_click_time < 0.5:
             if event.path.stem.startswith("sub-"):
@@ -70,6 +90,9 @@ class CreateFoldersTab(TabPane):
             sub_dir = self.query_one("#tabscreen_subject_input").value
             ses_dir = self.query_one("#tabscreen_session_input").value
 
+            if ses_dir == "":  # TODO: centralise this to func
+                ses_dir = None
+
             try:
                 self.project.make_folders(
                     sub_names=sub_dir,
@@ -80,3 +103,74 @@ class CreateFoldersTab(TabPane):
             except BaseException as e:
                 self.mainwindow.show_modal_error_dialog(str(e))
                 return
+
+    def run_local_validation(self, prefix):
+        """ """
+        try:
+            sub_dir = self.query_one("#tabscreen_subject_input").value
+
+            format_sub = formatting.check_and_format_names([sub_dir], "sub")
+
+            if prefix == "sub":
+                format_ses = None
+            else:
+                ses_dir = self.query_one("#tabscreen_session_input").value
+
+                format_ses = formatting.check_and_format_names(
+                    [ses_dir], "ses"
+                )
+
+            validation.validate_names_against_project(
+                self.project.cfg,
+                format_sub,
+                format_ses,
+                local_only=True,
+                error_or_warn="error",
+                log=False,
+            )
+
+        except Exception as e:
+            return False, str(e)
+
+        return True, "No issues detected"
+
+    def update_input_tooltip(self, message: Optional[str], prefix):
+        """ """
+        id = (
+            "#tabscreen_subject_input"
+            if prefix == "sub"
+            else "#tabscreen_session_input"
+        )
+        input = self.query_one(id)
+        input.tooltip = message
+
+
+class QuickNeuroBlueprintValidator(Validator):
+    """"""
+
+    def __init__(self, prefix, parent):
+        self.parent = parent
+        self.prefix = prefix
+
+    def validate(self, name: str) -> ValidationResult:
+        """"""
+        valid, message = self.parent.run_local_validation(self.prefix)
+
+        self.parent.update_input_tooltip(message, self.prefix)
+
+        if valid:
+            if self.prefix == "sub":
+                # re-validate the ses in case the new sub has made it ok.
+                value = self.parent.query_one("#tabscreen_session_input").value
+                self.parent.query_one("#tabscreen_session_input").validate(
+                    value=value
+                )
+
+            return self.success()
+        else:
+            return self.failure("")
+
+
+# it is allowing double keys, if not prefix
+# ses-a etc is not checked for sub name...
+# weird focus on
