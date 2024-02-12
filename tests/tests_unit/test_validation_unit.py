7464add333b5ff9bc81aa4c82c836d1ce677f2b7
JoeZiminski <joseph.j.ziminski@gmail.com>
2024-02-12 22:40:32 +0000

    Check all special characters not just spaces during validation.

--- tests/tests_unit/test_validation_unit.py
+++ tests/tests_unit/test_validation_unit.py
@@ -53,19 +53,24 @@ class TestValidationUnit:
         [
             ["sub", ["sub- 001"]],
             ["sub", ["sub-1992_@DATE@_id 1232", "sub-1993-id_1234"]],
-            ["ses", ["ses-001", "ses-00 2"]],
+            ["ses", ["ses-001!", "ses-00 2"]],
+            ["ses", ["ses-#001", "ses-00 2"]],
+            ["ses", ["ses-001!", "ses-00%2"]],
         ],
     )
-    def test_spaces_in_format_names(self, prefix_and_names):
+    def test_special_characters_in_format_names(self, prefix_and_names):
         """
         Check `validate_list_of_names()` catches
-        spaces in passed names.
+        spaces in passed names (not all names are bad
         """
         prefix, names = prefix_and_names
         with pytest.raises(BaseException) as e:
             validation.validate_list_of_names(names, prefix)
 
-        assert "Spaces are in the name:" in str(e.value)
+        assert (
+            "contains characters which are not alphanumeric, dash or underscore."
+            in str(e.value)
+        )
 
     @pytest.mark.parametrize("prefix", ["sub", "ses"])
     def test_formatting_dashes_and_underscore_alternate_incorrectly(
@@ -97,7 +102,7 @@ class TestValidationUnit:
                 [
                     f"{prefix}-001",
                     f"{prefix}-002_ses-002-task-check",
-                    f"{prefix}-003_@DATE@",
+                    f"{prefix}-003_date-123123",
                 ],
                 [f"{prefix}-002_ses-002-task-check"],
             ],
@@ -120,10 +125,12 @@ class TestValidationUnit:
             with pytest.raises(BaseException) as e:
                 validation.validate_list_of_names(all_names, f"{prefix}")
 
-            assert error_message(bad_names) in str(
-                e.value
-            ), f"failed: {all_names}"
-
+            try:
+                assert error_message(bad_names) in str(
+                    e.value
+                ), f"failed: {all_names}"
+            except:
+                breakpoint()
         # check these don't raise
         all_names = [f"{prefix}-001_hello-world_one-hundred"]
         validation.validate_list_of_names(all_names, f"{prefix}")
