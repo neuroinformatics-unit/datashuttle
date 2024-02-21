428d26b53f5c8c42a4ef1c820f10b31558290024
JoeZiminski <joseph.j.ziminski@gmail.com>
2024-02-21 12:48:20 +0000

    Add code to distinguish between macos and windows.

--- datashuttle/tui/run_app.py
+++ datashuttle/tui/run_app.py
@@ -1,26 +1,62 @@
 import os
 import subprocess
 from pathlib import Path
+from sys import platform
 
 dir_path = Path(os.path.dirname(os.path.realpath(__file__))).as_posix()
 
 my_env = os.environ.copy()
 
-# TODO: if on windows, suggest they install windows terminal
+# TODO: deal with font size
+if platform == "windows":
+    default_prog_line = ""
+    font_line = font = (
+        """wezterm.font({family = "Cascadia Mono", weight="DemiLight" }),"""
+    )
+elif platform == "darwin":
+    default_prog_line = (
+        "default_prog = { 'bash', '-l', '-c', 'conda activate datashuttle && python "
+        + f"{dir_path}"
+        + "/app.py' },"
+    )
+    font_line = ""
+else:
+    default_prog_line = ""
+    font_line = ""
 
 my_str = """ """
 for key, value in os.environ.items():
-    if "CONDA" in key or key == "PATH":
+    if "(" in key:  # "CONDA" in key or key == "PATH":
         my_str += f"""
             {key}='{Path(value).as_posix()}',"""
+
+#      font = wezterm.font({family = "Cascadia Mono", weight="DemiLight" }),
+default_prog = (
+    {
+        "zsh",
+        "-l",
+        "-c",
+        "conda activate datashuttle && python /Users/joeziminski/git_repos/datashuttle/datashuttle/tui/app.py",
+    },
+)
+
+#     exit_behavior = "Hold"
+#      default_prog = { 'zsh', '-l', '-c', 'source activate datashuttle && conda activate datashuttle && python /Users/joeziminski/git_repos/datashuttle/datashuttle/tui/app.py' },,
+# TODO: probably cannot rely on default prog is bash, but using zsh (even though setup for zsh) requires shell init... need to test more cross-platform.
+# Could ask on wezterm forum too
 message = (
     """
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
 return {
-     font = wezterm.font({family = "Cascadia Mono", weight="DemiLight" }),
-     font_size = 10.0,
+     font_size = 14.0,
+     """
+    + default_prog_line
+    + """
+     """
+    + font_line
+    + """
      set_environment_variables = { """
     + my_str
     + """
@@ -28,12 +64,10 @@ return {
 }
 """
 )
-#     exit_behavior = "Hold",
+
 with open(f"{dir_path}/wezterm/.wezterm.lua", "w") as text_file:
     text_file.write(message)
 
-print("start conda run -n datashuttle {dir_path.as_posix()}/app.py")
-subprocess.Popen(
-    f"{dir_path}/wezterm/wezterm.exe --config-file {dir_path}/wezterm/.wezterm.lua start conda activate datashuttle && python {dir_path}/app.py",
-    env=my_env,
-)  # noqa
+my_env["WEZTERM_CONFIG_FILE"] = f"{dir_path}/wezterm/.wezterm.lua"
+my_env["CONDA_AUTO_ACTIVATE_BASE"] = "true"
+subprocess.Popen(["open", f"{dir_path}/wezterm/WezTerm.app"], env=my_env)
