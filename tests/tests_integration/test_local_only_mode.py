13f7a3bf1feacb01ce980ad85e2a5c45d0e691e4
JoeZiminski <joseph.j.ziminski@gmail.com>
2025-02-05 23:23:04 +0000

    Add a new test and remove redundant fixture.

--- tests/tests_integration/test_local_only_mode.py
+++ tests/tests_integration/test_local_only_mode.py
@@ -1,4 +1,3 @@
-import os.path
 import shutil
 
 import pytest
@@ -15,21 +14,6 @@ TEST_PROJECT_NAME = "test_project"
 
 class TestLocalOnlyProject(BaseTest):
 
-    @pytest.fixture(scope="function")
-    def local_project(self, tmp_path):
-        """
-        Use a local-only project as a fixture. This project
-        as only the local path set, all connection-related configs are `None`.
-        """
-        tmp_path = tmp_path / "test_local"
-
-        project = DataShuttle(TEST_PROJECT_NAME)
-        project.make_config_file(local_path=tmp_path)
-
-        cwd = os.getcwd()
-        yield project
-        test_utils.teardown_project(cwd, project)
-
     def test_bad_setup(self, tmp_path):
         """
         Test setup without providing both central_path and connection
@@ -37,7 +21,7 @@ class TestLocalOnlyProject(BaseTest):
         """
         local_path = tmp_path / "test_local"
 
-        project = DataShuttle(TEST_PROJECT_NAME)
+        project = DataShuttle("this_project_is_not_torn_down")
 
         with pytest.raises(ConfigError) as e:
             project.make_config_file(
@@ -55,6 +39,7 @@ class TestLocalOnlyProject(BaseTest):
             in str(e.value)
         )
 
+    @pytest.mark.parametrize("project", ["local"], indirect=True)
     def test_full_to_local_project(self, project):
         """
         Make a full project a local-only project, and check the transfer
@@ -73,28 +58,57 @@ class TestLocalOnlyProject(BaseTest):
 
         project.validate_project("rawdata", "error")
 
-    def test_local_to_full_project(self, local_project):
+    @pytest.mark.parametrize("project", ["local"], indirect=True)
+    def test_local_project_to_full(self, tmp_path, project):
+        """
+        Test updating a local-only project to a full one
+        by adding the required configs (both must be set together)
+        Perform a quick check that data transfer does not error out
+        now that the project is full, and the configs are set as expected.
+        """
+        central_path = tmp_path / "central"
+        connection_method = "local_filesystem"
+
+        # check must set both at once
+        with pytest.raises(ConfigError):
+            project.update_config_file(central_path=central_path)
+
+        with pytest.raises(ConfigError):
+            project.update_config_file(connection_method=connection_method)
+
+        project.update_config_file(
+            central_path=central_path, connection_method=connection_method
+        )
+
+        # smoke test, should not raise now is a full project.
+        project.upload_entire_project()
+
+        assert project.cfg["central_path"] == central_path / TEST_PROJECT_NAME
+        assert project.cfg["connection_method"] == connection_method
+
+    @pytest.mark.parametrize("project", ["local"], indirect=True)
+    def test_local_to_full_project(self, project):
         """
         Change a project from local-only to a normal project by updating
         the relevant configs. Smoke test that general functionality is maintained
         and that transfers work correctly.
         """
-        central_path = local_project.cfg["local_path"].parent / "central"
+        central_path = project.cfg["local_path"].parent / "central"
 
-        local_project.update_config_file(
+        project.update_config_file(
             central_path=central_path, connection_method="local_filesystem"
         )
 
-        paths_ = local_project.create_folders(
+        paths_ = project.create_folders(
             "rawdata", "sub-001", "ses-001", "ephys"
         )
         test_utils.write_file(
             paths_["ephys"][0] / "test_file", contents="test_entry"
         )
 
-        local_project.validate_project("rawdata", "error")
+        project.validate_project("rawdata", "error")
 
-        local_project.upload_entire_project()
+        project.upload_entire_project()
 
         assert (
             central_path
@@ -107,12 +121,13 @@ class TestLocalOnlyProject(BaseTest):
         ).is_file()
 
     @pytest.mark.parametrize("top_level_folder", ["rawdata", "derivatives"])
+    @pytest.mark.parametrize("project", ["full"], indirect=True)
     def test_get_next_sub_and_ses(self, project, top_level_folder):
         """
-        Make a project with subject and session > 1 in both local
-        and central projects. Then, delete the local and run get next sub / ses
-        explicitly requesting to also check central path. However, we are
-        in local-only mode so this request is ignored.
+        Make a 'full' project with subject and session > 1 in both local
+        and central projects. Then, delete the local and run get next sub / ses,
+        and make the project local-only. Call validation requesting to also
+        check central path, which should be ignored as we are in local-only mode.
         """
         test_utils.make_local_folders_with_files_in(
             project,
