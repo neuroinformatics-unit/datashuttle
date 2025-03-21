d44bf109dffe4348d8513b36ae17b8ce74880246
JoeZiminski <joseph.j.ziminski@gmail.com>
2025-03-21 01:01:25 +0000

    Fix validate at path class docstring.

--- datashuttle/tui/screens/validate_at_path.py
+++ datashuttle/tui/screens/validate_at_path.py
@@ -15,23 +15,10 @@ from datashuttle.tui.shared import validate_content
 
 class ValidateScreen(Screen):
     """
-    Screen for setting up a new datashuttle project, by
-    inputting the desired configs. This uses the
-    ConfigsContent window to display and set the configs.
-
-    If "Main Manu" button is pressed, the callback function
-    returns None, so the project screen is not switched to.
-
-    Otherwise, the logic for creating and validating the
-    project is in ConfigsContent. ConfigsContent calls
-    the dismiss method of this class to return
-    an initialised project to mainwindow.
-    See ConfigsContent.on_button_pressed() for more details
-
-    Parameters
-    ----------
-
-    mainwindow : TuiApp
+    Screen to hold the validation window for
+    validating an existing project at a given path.
+    All widgets are stored in `ValidateContent`, which is
+    shared between here and the validation tab on the project manager.
     """
 
     TITLE = "Validate Project"
