26ddc29a5ad863abf226e1b46f99e76ca11af43e
MoffittAndrew <moffittandrew@outlook.com>
2025-03-21 15:42:50 +0000

    moving Iterable import into type checking block to comply with TC003

--- datashuttle/tui/custom_widgets.py
+++ datashuttle/tui/custom_widgets.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from collections.abc import Iterable
 from typing import (
     TYPE_CHECKING,
     List,
@@ -10,6 +9,8 @@ from typing import (
 )
 
 if TYPE_CHECKING:
+    from collections.abc import Iterable
+    
     from textual import events
     from textual.validation import Validator
 
