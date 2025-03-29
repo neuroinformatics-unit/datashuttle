d5b1e3c5bf65186ec8339cc736888f518bf918ed
shrey <shrey@kali>
2025-03-30 04:19:25 +0530

    update: type checking for aws regions

--- datashuttle/configs/canonical_configs.py
+++ datashuttle/configs/canonical_configs.py
@@ -27,6 +27,7 @@ from pathlib import Path
 
 import typeguard
 
+from datashuttle.configs.regions import AWS_REGION
 from datashuttle.utils import folders, utils
 from datashuttle.utils.custom_exceptions import ConfigError
 
@@ -47,7 +48,7 @@ def get_canonical_configs() -> dict:
         "gdrive_client_id": Optional[str],
         "gdrive_client_secret": Optional[str],
         "aws_access_key_id": Optional[str],
-        "aws_s3_region": Optional[str],
+        "aws_s3_region": Optional[Literal[*AWS_REGION.get_all_regions()]],
         # "aws_s3_endpoint_url": Optional[str],
     }
 
