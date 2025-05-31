3cc7e0d092aace6181cb4e57e059f327fad675d0
shrey <shrey@kali>
2025-05-31 23:23:44 +0530

    refactor: aws_regions.py; provide aws secret access key at runtime

--- datashuttle/__init__.py
+++ datashuttle/__init__.py
@@ -2,7 +2,7 @@ from importlib.metadata import PackageNotFoundError, version
 
 from datashuttle.datashuttle_class import DataShuttle
 from datashuttle.datashuttle_functions import quick_validate_project
-from datashuttle.configs.regions import AWS_REGION
+from datashuttle.configs.aws_regions import AWS_REGION
 
 
 try:
