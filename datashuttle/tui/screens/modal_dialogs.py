03cc1971cff82cb22c6988a791d2a3b428374724
JoeZiminski <joseph.j.ziminski@gmail.com>
2025-07-30 22:05:04 +0100

    Revert "Keep default drive for SelectDirectoryTreeScreen as home."
    This reverts commit 60cf9ab85ff02cff0fc7f79251beec263d3f4083.

--- datashuttle/tui/screens/modal_dialogs.py
+++ datashuttle/tui/screens/modal_dialogs.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import asyncio
-from typing import TYPE_CHECKING, Callable
+from typing import TYPE_CHECKING, Callable, Optional
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -219,7 +219,9 @@ class SelectDirectoryTreeScreen(ModalScreen):
 
     """
 
-    def __init__(self, mainwindow: TuiApp) -> None:
+    def __init__(
+        self, mainwindow: TuiApp, path_: Optional[Path] = None
+    ) -> None:
         """Initialise SelectDirectoryTreeScreen.
 
         Parameters
@@ -234,13 +236,11 @@ class SelectDirectoryTreeScreen(ModalScreen):
         super(SelectDirectoryTreeScreen, self).__init__()
         self.mainwindow = mainwindow
 
-        self.path_ = Path().home()
+        if path_ is None:
+            path_ = Path().home()
+        self.path_ = path_
 
         self.click_info = ClickInfo()
-
-        # Flag as the Select triggers `on_select_change` during set up
-        # which results setting tree back to default drive not home.
-        self.skip_first_select_trigger = True
 
     def compose(self) -> ComposeResult:
         """Add widgets to the SelectDirectoryTreeScreen."""
@@ -305,13 +305,10 @@ class SelectDirectoryTreeScreen(ModalScreen):
 
     def on_select_changed(self, event: Select.Changed) -> None:
         """Update the directory tree when the drive is changed."""
-        if self.skip_first_select_trigger:
-            self.skip_first_select_trigger = False
-        else:
-            self.path_ = Path(event.value)
-            self.query_one(
-                "#select_directory_tree_directory_tree"
-            ).path = self.path_
+        self.path_ = Path(event.value)
+        self.query_one(
+            "#select_directory_tree_directory_tree"
+        ).path = self.path_
 
     @require_double_click
     def on_directory_tree_directory_selected(
