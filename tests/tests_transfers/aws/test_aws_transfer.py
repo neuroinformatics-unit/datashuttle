a175d0f09952e12c457452787efaf14aec0ab46d
JoeZiminski <Joseph.j.ziminski@gmail.com>
2025-09-04 15:20:10 +0100

    Skip AWS and GDRIVE tests when environment variables not set.

--- tests/tests_transfers/aws/test_aws_transfer.py
+++ tests/tests_transfers/aws/test_aws_transfer.py
@@ -2,10 +2,15 @@ import pytest
 
 from datashuttle.utils import rclone
 
+from ... import test_utils
 from ..base_transfer import BaseTransfer
 from . import aws_test_utils
 
 
+@pytest.mark.skipif(
+    not test_utils.has_aws_environment_variables(),
+    reason="AWS set up environment variables must be set.",
+)
 class TestAwsTransfer(BaseTransfer):
     @pytest.fixture(
         scope="class",
