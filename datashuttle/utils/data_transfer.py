8fbc4e644b5bd0de91a1cb043169e31bac84642c
JoeZiminski <joseph.j.ziminski@gmail.com>
2025-09-15 18:44:25 +0100

    Add comment to check_and_format_names.

--- datashuttle/utils/data_transfer.py
+++ datashuttle/utils/data_transfer.py
@@ -461,6 +461,10 @@ class TransferData:
                 processed_names += [f"all_non_{prefix}"]
 
         else:
+            # Format the sub and ses names to search for. This expands @T@
+            # flag and adds sub- or ses- prefixes if not found. Do not perform
+            # validation here as we want to be less restrictive during transfer
+            # searchers vs. folder creation.
             processed_names = formatting.check_and_format_names(
                 names_checked, prefix, bypass_validation=True
             )
