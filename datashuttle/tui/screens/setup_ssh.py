c8ae00b71ed9e2defa1a4aeffd42ef432c2a7d17
Novikov Kirill <75641500+knQzx@users.noreply.github.com>
2026-05-23 22:59:31 +0200

    clear password from TUI input after use (#717)
    * clear password from input after use and fix docstring typo
    
    * move password clear to after page transition

--- datashuttle/tui/screens/setup_ssh.py
+++ datashuttle/tui/screens/setup_ssh.py
@@ -190,6 +190,7 @@ class SetupSshScreen(ModalScreen):
             )
             self.failed_password_attempts += 1
 
+        self.query_one("#setup_ssh_password_input").value = ""
         self.query_one("#messagebox_message_label").update(message)
 
     def try_setup_rclone_encryption(self):
